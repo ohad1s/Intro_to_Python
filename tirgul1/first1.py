c01572d36102734c00dedbb8ebeff81d9c5bd66f
shirazi97 <shirazi1997@gmail.com>
2022-11-02 17:08:12 +0200

    t1

---
+++ tirgul1/first1.py
@@ -0,0 +1,67 @@
+8
+8.0
+"8"
+"8.0"
+True
+False
+[8,"fasle",False,"*", 7.7]
+x=8
+y=9
+y=10
+name="Ohad"
+last_name="Shirazi"
+isAlive = True
+Age="25"
+age2=25
+studentA=[name,last_name,age2,8798562,"two","22","kjvdslkg"]
+print(studentA)
+z=x+y
+w=z-x
+q=w/z
+e= q*z
+r=e//q
+t=e%y
+# 7**2
+# 8**3
+print(len(name))
+print(len(studentA))
+# print(len(25))
+first_letter=name[-1]
+print(first_letter)
+name_h=last_name[2:5]
+print(name_h)
+print(last_name[2::2])
+print(last_name[2:6:2])
+print(last_name[2:len(last_name)-1])
+print("Hello"+" 5")
+print("DAN"*4)
+print(studentA[2:7])
+name="Liron"
+# name[0]="R"
+name= 10
+print(name)
+name=input("Enter your name")
+print("Your name is:  "+name)
+age_3=input("enter age")
+age_4=int(age_3)
+# print("your age+10"+ age_3+10)
+
+number1=input("enter first")
+number2=input("enter second")
+print("first "+number1)
+print("second "+number2)
+temp=number1
+number1=number2
+number2=temp
+number1, number2 = number2 , number1
+print("first "+number1)
+print("second "+number2)
+
+
+
+
+
+
+
+
+
---
+++ tirgul1/first2.py
@@ -0,0 +1,72 @@
+# print("hello world")
+# 3
+# 3.0
+# "3"
+# True
+# False
+# x=10
+# y=20
+# name="Ohad"
+# last_name="Shirazi"
+# my_list=["ohad",25,"3.8",True]
+# z=10+x
+# w=z-2
+# t=x*z
+# q=2**t
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(len(last_name))
+# # print(last_name[12])
+# print(last_name[0:5])
+# print(last_name[0:4:2])
+# print(last_name[2:])
+# print(last_name[2:len(last_name)-1])
+# # print("dani"+5) #ex
+# print("dani"+"5")#daniyosssi
+# print("dan"*5)#dandandnadandna
+# # last_name[5]="y"
+# age=25
+# # age[1]
+# age=17
+# name2=input("Enter your name")
+# print("Hey "+name2+" How are you today?")
+# name2="yoossii"
+# print(name2)
+# temp=input("enter temp")
+# "25"
+# print(temp[0])
+# tmp_int=int(temp)
+# amount=str(66)
+# # print(tmp_int[0])
+# print(tmp_int+33)
+# print(amount[1])
+
+
+# print("num1: "+number1+" num2: "+number2)
+# number1=number2
+# number2=number1
+
+# temp_arg=number1
+# number1=number2
+# number2=temp_arg
+
+# number1, number2 = number2 , number1
+# x ,y ,z ,w = 4,7,8,9
+# print("num1: "+number1+" num2: "+number2)
+
+# number1=int(input("enter num1"))
+# number2=input("enter num2")
+# number2=int(number2)
+# number3=int(input("enter num3"))
+# avg=(number1+number2+number3)/3
+# print(avg)
+
+number4= input("enter number")
+print(number4[0])
+print(number4[1])
+number4= int(number4)
+
+
+
+
