0a24a9ec7a757e0d97d3c92e1cf3af71b0d366ec
shirazi97 <shirazi1997@gmail.com>
2022-12-01 13:43:07 +0200

    update 6

--- tirgul_6/csv_reading.py
+++ tirgul_6/csv_reading.py
@@ -1,25 +1,33 @@
 import csv
-open_file = open('./results.csv', encoding="utf-8")
+# import math
+# print(math.sqrt(9))
+open_file = open('./results.csv',encoding="utf-8")
+# open_file = open(r'C:\Users\shira\PycharmProjects\SemsterA_22-23\tirgul_6\csv_reading.py')
 # games_dict= csv.DictReader(open_file)
 games= csv.reader(open_file)
+# print(games)
 # for row in games:
 #     print(row)
+#     break
+
 print("------------------")
 games_data= list(games)
-print(games_data[0])
+# print(games_data)
 header= games_data[0]
-Icounter=0
-Igoals=0
-for game in games_data:
-    if game[1]=='Israel' or game[2]=="Israel":
-        print(game)
-        Icounter+=1
-    if game[1]=='Israel':
-        Igoals+=int(game[3])
-    if game[2] == 'Israel':
-        Igoals += int(game[4])
-print("Israel Goals: ", Igoals)
-print("Israel Games: ", Icounter)
-print("Israel avg goal per game: ", Igoals/Icounter)
+print(header)
+Isr_games_counter=0
+Isr_goals_counter=0
+for row in games_data:
+    if row[1]== 'Israel' or row[2]== "Israel":
+        print(row)
+        Isr_games_counter+=1
+    if row[1]=='Israel':
+        Isr_goals_counter+=(int(row[3]))
+    if row[2]=="Israel":
+        Isr_goals_counter+=(int(row[4]))
+print("Israel Goals: ", Isr_goals_counter)
+print("Israel Games: ", Isr_games_counter)
+print("Israel avg goal per game: ", Isr_goals_counter/Isr_games_counter)
+
 
 
