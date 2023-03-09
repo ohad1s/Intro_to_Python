6e82da6dcdb2d9d217d011e9181e6117b4a5e3eb
shirazi97 <shirazi1997@gmail.com>
2023-03-09 16:34:07 +0200

    first

---
+++ SemB/both/first.py
@@ -0,0 +1,34 @@
+# # int -> -7 , 0 ,8 ...
+# # str -> "ohad" , "5" "True", "-3", "bukjsefgkew"
+# # float -> 0.5 , 0.2 -0.3
+# # bool -> True, False
+# print("ohad")
+# # + - * / ** // %
+# # and or not in
+# print(2==2)
+# print(3!=3)
+# print(3!=2 and 2==1)
+# print(len("oh ad"))
+# # len(768) -> Error!
+# x=5
+# y="ohad"
+# z=y[1]
+# q=y[3]
+# zq= z+q
+# o=y[0]*10
+# # o2= y[0]+2 -> Error!
+# my_name= input("please enter your name")
+# fam_name= input("please enter your last name")
+# print("Hello "+ my_name+" "+fam_name)
+
+# num1 = float(input("enter first num"))
+# num2 = float(input("enter second num"))
+# num3 = float(input("enter third number"))
+# avg = (num1 + num2 + num3) / 3
+# print("your avg is: ", avg)
+
+c_temp= float(input("enter your c temp"))
+f_temp= c_temp*9/5 +32
+print("your f temp is: ",f_temp)
+
+
